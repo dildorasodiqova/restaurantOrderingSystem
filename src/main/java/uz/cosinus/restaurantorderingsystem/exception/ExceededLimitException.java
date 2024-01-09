361c82c1050740dba646d6f5c32d83959a99b845
Dildora <sodiqovadildora02@gmail.com>
2024-01-10 00:59:07 +0500

    ALL

--- src/main/java/uz/cosinus/restaurantorderingsystem/exception/ExceededLimitException.java
+++
@@ -1,6 +0,0 @@
-package uz.cosinus.restaurantorderingsystem.exception;
-public class ExceededLimitException extends RuntimeException{
-    public ExceededLimitException(String message){
-        super(message);
-    }
-}
